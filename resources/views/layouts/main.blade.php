996d84f740309a1629364d3649bb655979c916eb
torika2 <torikabot@gmail.com>
2021-11-15 19:17:01 +0400

    1

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,49 +8,45 @@
         <link rel="stylesheet" type="text/css" href="/css/app.css">
         <link rel="stylesheet" type="text/css" href="/css/costum.css">
     </head>
-    <body class="">
-      <div class="col-3 ">
-        <div class="sidebar pl-4">
-          <div>
-            <img src="/logo.svg">
-          </div>
-          <div class="menubar">
-            <div>
-              <ul class="das-menu">
-                <li>
-                    <a href="https://www.startupcentraleurasia.com/en/dashboard/" @if(str_contains(url()->current(), "/startup/dashboard")) class="active" @endif>
-                        <img src="https://www.startupcentraleurasia.com/themes/application/Components/svg/dashboard/dashboard.svg" alt="">
-                        <span>Dashboard</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#" @if(str_contains(url()->current(), "/startup/offers")) class="active" @endif>
-                        <img src="https://www.startupcentraleurasia.com/themes/application/Components/svg/dashboard/get-investment.svg" alt="">
-                        <span>Offers</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.startupcentraleurasia.com/en/dashboard/startUpAccount" @if(str_contains(url()->current(), "/startup/account")) class="active" @endif>
-                        <img src="https://www.startupcentraleurasia.com/themes/application/Components/svg/dashboard/myaccount.svg" alt="">
-                        <span>My account</span>
-                    </a>
-                </li>
-              </ul>
+    <body class="row">
+        <div class="col-3">
+            <div class="sidebar pl-4">
+              <div>
+                <img src="/logo.svg">
+              </div>
+              <div class="menubar">
+                <div>
+                  <ul class="das-menu">
+                    <li>
+                        <a href="https://www.startupcentraleurasia.com/en/dashboard/">
+                            <img src="https://www.startupcentraleurasia.com/themes/application/Components/svg/dashboard/dashboard.svg" alt="">
+                            <span>Dashboard</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src="https://www.startupcentraleurasia.com/themes/application/Components/svg/dashboard/get-investment.svg" alt="">
+                            <span>Offers</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.startupcentraleurasia.com/en/dashboard/startUpAccount">
+                            <img src="https://www.startupcentraleurasia.com/themes/application/Components/svg/dashboard/myaccount.svg" alt="">
+                            <span>My account</span>
+                        </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div class="sidebar-logout">
+                <img src="/assets/images/power-off.png" alt="" height="30px" style="opacity:0.6">
+                Logout
+              </div>
             </div>
-          </div>
-          <div class="sidebar-logout">
-            <img src="/assets/images/power-off.png" alt="" height="30px" style="opacity:0.6">
-
-            Logout
-          </div>
-          
         </div>
-        
-      </div>
-      <div id="app" class="col-9">
-        @yield("content")
-      </div>
-
+        <div id="app" class="col-9">
+            @yield("content")
+        </div>
     <script type="text/javascript" src="/js/app.js"></script>
     </body>
 </html>
