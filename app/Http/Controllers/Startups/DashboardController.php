01ee5e85d7ee93d0c8c4e0f9d6fa21439f283621
MrDarkG <vako.batiashvili1998@gmail.com>
2021-11-15 19:18:54 +0400

    a

--- app/Http/Controllers/Startups/DashboardController.php
+++ app/Http/Controllers/Startups/DashboardController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers\Startups;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Investor;
+use App\Models\Investor_type;
 
 class DashboardController extends Controller
 {
     public function index($value='')
     {
+        $types=Investor_type::get();
         $investors=Investor::allInvestors();
         
         return view("startup.dashboard",[
             'investors' => $investors,
+            'types' => $types
         ]);
     }
 }
