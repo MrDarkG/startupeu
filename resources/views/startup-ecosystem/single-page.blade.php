93c4ddd0a6a265b4e2208292e67dc85c9f36949c
torika <torikabot@gmail.com>
2022-09-22 20:53:18 +0400

    feat: database.add\faq_categories, faq_questions, faq_answers\ Startup_ecosystem

--- resources/views/startup-ecosystem/single-page.blade.php
+++ resources/views/startup-ecosystem/single-page.blade.php
@@ -19,7 +19,7 @@
                 </h1>
             </div>
             <div class="mb-3 mt-5 pt-5">
-                {{ serialize($startup_ecosystem->pdf)[0] }}
+                {{ serialize($startup_ecosystem->pdf) }}
                 <a href="/uploads/">
                     <div
                         class="btn pl-4 pr-4"
