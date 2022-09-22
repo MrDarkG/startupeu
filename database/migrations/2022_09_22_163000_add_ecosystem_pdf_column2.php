049b50d163aebf14818ea82b08c5ec14e0ccb30f
torika <torikabot@gmail.com>
2022-09-22 20:30:50 +0400

    4

---
+++ database/migrations/2022_09_22_163000_add_ecosystem_pdf_column2.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEcosystemPdfColumn2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Startup_ecosystem', function (Blueprint $table) {
+            //
+            $table->string('pdf');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Startup_ecosystem', function (Blueprint $table) {
+            //
+        });
+    }
+}
