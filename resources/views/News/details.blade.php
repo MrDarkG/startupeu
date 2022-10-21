e4e4129753a49c2cc52c026bc798878a8be0a5af
torika <torikabot@gmail.com>
2022-10-21 13:48:13 +0400

    feat: news single-page

--- resources/views/News/details.blade.php
+++ resources/views/News/details.blade.php
@@ -8,19 +8,24 @@
     <section class="main-section container">
         <div class="row news-details justify-content-between">
             <div class="col-md-9">
-                <h1>
+                <h1 class="mt-3 mb-3">
                     {{ $news->title }}
                 </h1>
+                <div class="mt-3 mb-3">
+                    <small  class="mt-2 mb-2" style="font-size: 15px;">
+                        {{ $news->created_at->format("d M, Y") }}
+                    </small>
+                </div>
+            </div>
+            <div class="col-md-3"></div>
+            <div class="col-md-9">
                 <img src="{{asset('uploads').'/'.$news->image}}" alt="{{ $news->title }}" width="100%">
-                <small>
-                    {{ $news->created_at->format("d M, Y") }}
-                </small>
                 <div class="news-single-description">
                     {!! $news->text !!}
                 </div>
             </div>
-            <div class="col-md-3">
-                <span class="more-news-text mt-0 mb-0">More news</span>
+            <div class="col-md-3 ">
+                <h2 class="mt-0 mb-0 font-weight-bold text-black m-0" style="font-size: 22px;">More news</h2>
                 @foreach($otherNews as $other)
                     <div >
                         <a class="news-more-item" href="{{ route("news.details", [
@@ -36,7 +41,7 @@
                             </h4>
                         </a>
                     </div>
-               @endforeach
+                @endforeach
             </div>
         </div>
     </section>
