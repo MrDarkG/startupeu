c18b14074f259287020139e30178bfd3587249a0
torika <torikabot@gmail.com>
2022-10-28 02:55:19 +0400

    1

--- resources/views/layouts/guest/guest.blade.php
+++ resources/views/layouts/guest/guest.blade.php
@@ -16,14 +16,48 @@
                 color: white!important;
             }
         </style>
+        <!-- Meta Pixel Code -->
+        <script>
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '864977757817740');
+            fbq('track', 'PageView');
+        </script>
+        <!-- Google Tag Manager -->
+        <script>
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PCVJ524');
+        </script>
+        <!-- End Google Tag Manager -->
+        <noscript>
+            <img height="1" width="1" style="display:none"src="https://www.facebook.com/tr?id=864977757817740&ev=PageView&noscript=1"
+            /></noscript>
+        <!-- End Meta Pixel Code -->
     </head>
     <body style="height: 100%!important;" class="d-flex flex-column justify-content-between">
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164883851-1"></script>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PCVJ524"height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
+
         gtag('config', 'UA-164883851-1');
+    </script>
+    <script>
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
             h._hjSettings={hjid:1763993,hjsv:6};
