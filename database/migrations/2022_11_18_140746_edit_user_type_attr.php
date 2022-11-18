2cd1a53dadcd35c83d27e2b212351bde00c3251a
torika <torikabot@gmail.com>
2022-11-18 18:09:43 +0400

    update_user_type_attr

--- database/migrations/2022_11_18_140746_edit_user_type_attr.php
+++ database/migrations/2022_11_18_140746_edit_user_type_attr.php
@@ -14,7 +14,7 @@ class EditUserTypeAttr extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('user_type')->nullable()->change();
         });
     }
 
