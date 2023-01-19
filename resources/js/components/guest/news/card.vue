02155c52fa12fbf5c226e189c28ffd11799fb15d
torika <torikabot@gmail.com>
2023-01-19 15:32:13 +0400

    feat: fix countries bug | production

--- resources/js/components/guest/news/card.vue
+++ resources/js/components/guest/news/card.vue
@@ -12,7 +12,7 @@
                     class="news-single-image d-flex p-0 w-100 rounded-top"
                 ></div>
                 <div
-                    class="d-flex pl-4 pr-4 pt-3 pb-3 flex-column align-items-start justify-content-between"
+                    class="d-flex pl-4 pr-4 pt-3 mb-2 flex-column align-items-start justify-content-between"
                     style="height: 50%;"
                 >
                     <div
@@ -51,7 +51,6 @@ export default {
             http.send()
 
             return (http.status != 404)?image_url:'/mini-logo.svg'
-
         }
     }
 }
