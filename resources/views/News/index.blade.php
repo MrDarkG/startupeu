203cc5b1183b60b85fd6d36bb5709a1e1aff539a
torika <torikabot@gmail.com>
2023-01-19 13:55:17 +0400

    feat: fix countries bug | production

--- resources/views/News/index.blade.php
+++ resources/views/News/index.blade.php
@@ -4,6 +4,14 @@
         color: blue;
         text-decoration: underline;
     }
+    .news-single-image{
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        height: 50%;
+        border-bottom-right-radius: unset !important;
+        border-bottom-left-radius: unset !important;
+    }
 </style>
 @section("content")
     <div class="row container m-auto pt-3 pb-3 pr-2 pl-2">
@@ -15,13 +23,9 @@
                 <a href="/news/{{urlencode($item->title)}}.{{$item->id}}">
                     <div class="bg-white d-flex flex-column h-100 rounded-2 overflow-hidden" style="box-shadow: 0px 4px 32px rgb(0 0 0 / 16%);">
                         <div
-                            class="d-flex p-0 w-100 rounded rounded-2"
+                            class="d-flex p-0 w-100 rounded rounded-2 news-single-image"
                             style="
                                 background-image: url('/uploads/{{$item->image}}');
-                                background-repeat: no-repeat;
-                                background-size: cover;
-                                background-position: center;
-                                height: 50%;
                             "
                         ></div>
                         <div
