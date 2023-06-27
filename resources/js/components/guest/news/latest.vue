7a01dd5df6506771a051ebcaf1f38e4002ab5755
torika <torikabot@gmail.com>
2023-06-27 20:28:23 +0400

    feat: PROD

--- resources/js/components/guest/news/latest.vue
+++ resources/js/components/guest/news/latest.vue
@@ -1,50 +1,49 @@
 <template>
 <div class="row pr-2 pl-2">
 	<div
-		v-for="(item, item_i) in news"
+        v-for="(item, item_i) in news"
         class="col-md-12 pl-4 pr-4"
-        :class="{
-            'col-sm-12 mt-3 col-md-6 col-lg-4':item_i > 0 || item_i === 0 && !checkScreenSize()
-        }"
+        v-if="item_i === 0 && checkScreenSize()"
         :style="{
             height:'280px'
         }"
 	>
-		<a
-            :href="`/news/${item.id}`"
-            class="p-4"
-        >
-			<div class="bg-white row h-100 rounded-2 overflow-hidden" style="box-shadow: 0px 4px 32px rgb(0 0 0 / 16%);">
-				<div
-                    :class="{
-                        'col-md-6':item_i === 0 && checkScreenSize(),
-                        'col-md-12':item_i > 0
-				    }"
-                    class="p-0 rounded rounded-2"
-                >
-					<img
+        <a :href="`/news/${item.id}`">
+            <div class="bg-white row h-100 rounded-2 overflow-hidden" style="box-shadow: 0px 4px 32px rgb(0 0 0 / 16%);">
+                <div class="p-0 rounded rounded-2 col-md-6">
+                    <img
                         class="col-md-12 h-100 p-0"
                         :src="`/uploads/${item.image}`" :alt="item.image"
                     >
-				</div>
-				<div
-					class="col-md-6 d-flex pl-4 pr-4 pt-3 pb-3 flex-column align-items-start h-100 justify-content-between"
-				>
-					<div v-if="item.created_at" class="text-black-50">
-						{{ setCustomDate(item.created_at) }}
-					</div>
-					<div class="pt-2">
-						<b>
-							<h4 class="m-0 p-0" v-text="item.title" style="font-size: 22px!important;"></h4>
-						</b>
-					</div>
-					<div class="pt-2">
-						<h6 class="text-black-50 latestnews-desc" v-html="item.description" style="font-size:16px!important;"></h6>
-					</div>
-				</div>
-			</div>
-		</a>
+                </div>
+                <div
+                    class="col-md-6 d-flex pl-4 pr-4 pt-3 pb-3 flex-column align-items-start h-100 justify-content-between"
+                >
+                    <div v-if="item.created_at" class="text-black-50">
+                        {{ setCustomDate(item.created_at) }}
+                    </div>
+                    <div class="pt-2">
+                        <b>
+                            <h4 class="m-0 p-0" v-text="item.title" style="font-size: 22px!important;"></h4>
+                        </b>
+                    </div>
+                    <div class="pt-2">
+                        <h6 class="text-black-50 latestnews-desc" v-html="item.description" style="font-size:16px!important;"></h6>
+                    </div>
+                </div>
+            </div>
+        </a>
 	</div>
+    <news-card
+        v-else-if="item_i === 0 && checkScreenSize()"
+        class="col-12 col-sm-12 col-md-6 col-lg-4"
+        :item="item"
+    ></news-card>
+    <news-card
+        v-else-if="item_i > 0"
+        class="col-12 col-sm-12 col-md-6 col-lg-4"
+        :item="item"
+    ></news-card>
 </div>
 </template>
 <script>
