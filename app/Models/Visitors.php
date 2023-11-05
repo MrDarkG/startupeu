ab22218b9ecfd746df22d6eb2738b1002a772036
torika <torikabot@gmail.com>
2023-11-05 18:55:51 +0400

    feat: Migration

---
+++ app/Models/Visitors.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @method static truncate()
+ */
+class Visitors extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'ip',
+        'referer'
+    ];
+}
